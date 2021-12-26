c73687f7f0522f576ccaf1353a416192efe2e353
LAPTOP-HQ5O5L46\陳泂杋 <leo082060826@gmail.com>
2021-12-26 22:20:44 +0800

    feat: scene1

--- VRGPproject/Assets/Scripts/scene1/Scene1Glowing.cs
+++ VRGPproject/Assets/Scripts/scene1/Scene1Glowing.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Scene1Glowing : MonoBehaviour
 {
+    public float speed = 1f;
     public Glow [] glowObjs;
 
     void Update()
     {
-        float timeArg = Mathf.Abs(Mathf.Sin(Time.deltaTime));
+        float timeArg = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup*speed));
         for(int i=0; i<glowObjs.Length; i++)
         {
             glowObjs[i].glowStr = timeArg;
